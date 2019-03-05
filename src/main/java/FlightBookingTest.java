73eb1af55d7fe0434c414d25d1b9e80458ed5ab0
Uday Seshadri <uday_seshadri@mcafee.com>
2019-03-05 08:15:14 +0530

    Fixed the First Script for success

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -26,20 +26,20 @@ public class FlightBookingTest {
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+        driver.findElement(By.id("FromTag")).sendKeys("bang");
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        waitFor(5000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
+        waitFor(5000);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
